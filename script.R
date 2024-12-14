ea349c460768ff1197507e2aca99060228cd66d1
Farheen Fathima <farheennfathima@gmail.com>
2024-12-14 10:48:35 +0530

    script

---
+++ script.R
@@ -0,0 +1,25 @@
+# The basics of R programming
+# Explore
+# Clean
+# Manipulate
+# Describe
+# Visualize
+# Analyse
+
+# R studio
+############
+# The four quadrants
+# How to get help when you need it
+?mean
+?ChickWeight
+
+# Objects and functions
+5 + 6
+
+a <- 5
+b <- 6
+
+a + b
+
+sum(a, b)
+# 4:36
