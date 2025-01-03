258179a6d42b3596fba58c682cab28f884e2b2cb
Farheen Fathima <farheennfathima@gmail.com>
2025-01-03 19:20:11 +0530

    changes

--- script.R
+++ script.R
@@ -203,3 +203,90 @@ View(long_data)
 View(msleep)
 
 # Range / Spread
+min(msleep$awake)
+max(msleep$awake)
+range(msleep$awake)
+IQR(msleep$awake)
+
+# Centrality
+mean(msleep$awake)
+median(msleep$awake)
+mode(msleep$awake)
+
+# Variance
+var(msleep$awake)
+
+summary(msleep$awake)
+
+# 2 variable format
+msleep %>% 
+  select(awake, sleep_total) %>% 
+  summary()
+
+# Summarizing the data
+msleep %>% 
+  drop_na(vore) %>% 
+  group_by(vore) %>% 
+  summarise(Lower = min(sleep_total),
+            Average = mean(sleep_total),
+            Upper = max(sleep_total),
+            Difference = max(sleep_total)-min(sleep_total)) %>% 
+  arrange(Average) %>% 
+  View()
+
+# Create Tables
+table(msleep$vore)
+
+msleep %>% 
+  select(vore, order) %>% 
+  filter(order %in% c("Rodentia", "Primates")) %>% 
+  table()
+
+# Visualize
+###############
+
+plot(pressure)
+
+# The grammar of graphics
+  # data
+  # mapping (X and Y axis variables)
+  # geometry (type of graph)
+
+# Bar plots
+ggplot(data = starwars,
+       mapping = aes(x = gender))+
+  geom_bar()
+
+# Histograms
+starwars %>% 
+  drop_na(height) %>% 
+  ggplot(mapping = aes(x = height))+
+  geom_histogram()
+
+# Box plots
+starwars %>% 
+  drop_na(height) %>% 
+  ggplot(aes(height))+
+  geom_boxplot(fill = "steelblue")+
+  theme_bw()+
+  labs(title = "Boxplot of height",
+       x = "Height of characters")
+
+# Density plots
+starwars %>% 
+  drop_na(height) %>% 
+  filter(sex %in% c("male", "female")) %>% 
+  ggplot(aes(height, 
+             color = sex,
+             fill = sex))+
+  geom_density(alpha = 0.4)+
+  theme_bw()
+
+# Scatter plots
+starwars %>% 
+  filter(mass < 200) %>% 
+  ggplot(aes(height, mass, color = sex))+
+  geom_point(size = 5, alpha = 0.5)+
+  theme_bw()+
+  labs(title = "Height and mass by sex")
+
